9a6f1d6a1a763a07dd8a56413b308adff5627b7b
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-06 03:34:38 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase= "Pretty bird!")
+def parrot(string= "Pretty bird!")
   puts "Squawk!"
     "Squawk!"
  
